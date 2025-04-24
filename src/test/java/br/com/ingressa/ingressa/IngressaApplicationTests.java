48f87a97eaadee3a42eb396d048cfb97a5dd2674
Lucas Silva <lucas.sca2022@gmail.com>
2025-04-24 12:05:26 -0300

    first commit

---
+++ src/test/java/br/com/ingressa/ingressa/IngressaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ingressa.ingressa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngressaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
