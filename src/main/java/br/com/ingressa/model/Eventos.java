054a07bd6ae429542ce345b9545c2063f2781852
Lucas Silva <lucas.sca2022@gmail.com>
2025-05-01 21:02:14 -0300

    Adição de model eventos, criação da tabela no bd para eventos

---
+++ src/main/java/br/com/ingressa/model/Eventos.java
@@ -0,0 +1,44 @@
+package br.com.ingressa.model;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Entity(name = "eventos")
+public class Eventos {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = true, length = 30, name = "nome_evento")
+    private String nome_evento;
+
+    @Column(nullable = true, name = "horario_evento", columnDefinition = "time")
+    private LocalTime horario_evento;
+
+    @Column(nullable = true, name = "data_evento", columnDefinition = "date")
+    private LocalDate data_evento;
+
+    @Column(nullable = true, name = "preco_evento", columnDefinition = "decimal(10,2)")
+    private BigDecimal preco_evento;
+
+    @Column(nullable = true, name = "descricao_evento", columnDefinition = "varchar(255)")
+    private String descricao_evento;
+
+    @Column(nullable = true, name = "rua_evento", columnDefinition = "varchar(255)")
+    private String rua_evento;
+
+    @Column(nullable = true, name = "cidade_evento", columnDefinition = "varchar(255)")
+    private String cidade_evento;
+
+    @Column(nullable = true, name = "estado_evento", columnDefinition = "varchar(2)")
+    private String estado_evento;
+
+    @Column(nullable = true, name = "numero_evento", columnDefinition = "int")
+    private int numero_evento;
+
+    @Column(nullable = true, name = "foto_evento", columnDefinition = "longblob")
+    private byte[] foto_evento;
+}
